4e78b388adcf80c50ae25ef3c837510a35460aa6
Sasha Karachev <shuricans@gmail.com>
2021-10-12 00:03:12 +0300

    first task completed, may be...

--- src/main/java/lesson2/HW/Solution.java
+++ src/main/java/lesson2/HW/Solution.java
@@ -1,11 +1,10 @@
 package lesson2.HW;
 
-import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Solution {
     final static int[] testArr;
-    final static int SIZE = 2;
+    final static int SIZE = 1_000_000;
 
     static {
         testArr = new int[SIZE];
@@ -18,56 +17,59 @@ public class Solution {
                 testArr[i] = i + 2;
             }
         }
-        System.out.println(Arrays.toString(testArr));
     }
 
     public static void main(String[] args) {
-//         5
-//        int[] a = {1, 2, 3, 4, 6};
-//        System.out.println(5);
-//        System.out.println(Arrays.toString(a));
-        int[] a = new int[0];
-        System.out.println(searchMissedValue(a));
+        System.out.println(searchMissedValue(testArr));
     }
 
-
-
+    /**
+     * Finds the missing element in an {@code arr}, see examples and description.
+     * Based on binary search.
+     * Complexity O(log n).
+     * Sequence in {@code arr}:
+     * - started from 1
+     * - arr[n] = arr[n-1] + 1
+     * <p>
+     * Examples:
+     * [1, 2 ,3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16] => 11
+     * [1, 2, 4, 5, 6] => 3
+     * [] => 1
+     *
+     * @param arr - array of integers.
+     * @return missed value or -1 if no missing element in the array.
+     */
     public static int searchMissedValue(int[] arr) {
 
-        // base case for empty array
-        if (arr.length == 0) {
+        // simple base cases
+        // empty array, first element not equals one
+        if (arr.length == 0 || arr[0] != 1) {
             return 1;
         }
 
         int start = 0;
         int end = arr.length - 1;
         int base;
-        int i = 0;
 
         while (end >= start) {
-            i++;
-            base = (start + end) / 2;
-            System.out.println("Base: " + base);
 
-            if (arr[base] == base + 1) { // go to the right part
+            base = (start + end) / 2;
+
+            if (arr[base] == base + 1) { // true - means the missing element will be on the right side
                 start = base + 1;
-                if (arr[start] > base + 2) {
-                    System.out.println("Кол-во итераций (1): " + i);
-                    return base + 2;
+                // if new start element greater then his index + 1, missed number is arr[start] - 1
+                if (arr[start] > start + 1) {
+                    return arr[start] - 1;
                 }
-            } else if(arr[base] > base + 1 && end > start) { // go to the left part
-                if(arr[base - 1] == base) {
-                    System.out.println("Кол-во итераций (2): " + i);
-                    return base + 1;
-                }
+            } else if (arr[base] > base + 1) { // true - means the missing element will be on the left side
                 end = base - 1;
-            } else {
-                System.out.println("Кол-во итераций (3): " + i);
-                return base + 1;
+                // if new end element equals his index + 1, missed number is arr[end] + 1
+                if (arr[end] == end + 1) {
+                    return arr[end] + 1;
+                }
             }
         }
-
-        System.out.println("Кол-во итераций: " + i);
+        // return -1 > no missing element in the array
         return -1;
     }
 }
--- src/test/java/lesson2/HW/SolutionTest.java
+++ src/test/java/lesson2/HW/SolutionTest.java
@@ -1,13 +1,66 @@
 package lesson2.HW;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.stream.Stream;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DisplayName("My simple parameterized testing")
 class SolutionTest {
 
-    @Test
-    void searchMissedValue() {
+    @ParameterizedTest
+    @MethodSource("dataProvider")
+    @DisplayName("searchMissedValue")
+    void searchMissedValue(int[] array, int expectedValue) {
+        assertEquals(expectedValue, Solution.searchMissedValue(array));
+    }
 
+    public static Stream<Arguments> dataProvider() {
+        return Stream.of(
+                Arguments.of(
+                        new int[]{},
+                        1
+                ),
+                Arguments.of(
+                        new int[]{2},
+                        1
+                ),
+                Arguments.of(
+                        new int[]{2, 3},
+                        1
+                ),
+                Arguments.of(
+                        new int[]{1, 3},
+                        2
+                ),
+                Arguments.of(
+                        new int[]{1, 2, 3, 5},
+                        4
+                ),
+                Arguments.of(
+                        getArray(10_000, 123),
+                        123
+                ),
+                Arguments.of(
+                        getArray(1_000_000, 89796),
+                        89796
+                )
+        );
+    }
+
+    private static int[] getArray(int length, int missedValue) {
+        final int[] a = new int[length];
+        for (int i = 0; i < a.length; i++) {
+            if (i + 1 < missedValue) {
+                a[i] = i + 1;
+            } else {
+                a[i] = i + 2;
+            }
+        }
+        return a;
     }
 }
