3121cef44c47f5751aa6837fd31f4d1bdf37a500
karachev.aleksandr <XN}O81Q.&AYX48F6^,@lbzo1xfiQMW,8KEP!^U'hg;~NtcB~>
2021-10-11 18:05:27 +0300

    sync

--- src/main/java/lesson2/search/BinarySearch.java
+++ src/main/java/lesson2/search/BinarySearch.java
@@ -3,6 +3,27 @@ package lesson2.search;
 public class BinarySearch {
     public static int search(Integer[] arr, int num) {
 
+        int start = 0;
+        int end = arr.length - 1;
+        int base;
+        int i = 0;
+
+        while (end >= start) {
+            i++;
+            base = (start + end) / 2;
+            System.out.println("Base: " + base);
+
+            if (arr[base] == num) {
+                System.out.println("Кол-во итераций: " + i);
+                return base;
+            } else if(arr[base] < num) {
+                start = base + 1;
+            } else {
+                end = base - 1;
+            }
+        }
+
+        System.out.println("Кол-во итераций: " + i);
         return -1;
     }
 }
--- src/main/java/lesson2/search/InterpolationSearch.java
+++ src/main/java/lesson2/search/InterpolationSearch.java
@@ -2,6 +2,35 @@ package lesson2.search;
 
 public class InterpolationSearch {
     public static int search(Integer[] arr, int num) {
+        int start = 0;
+        int end = arr.length - 1;
+        int base;
+        int i = 0;
+
+        while (end >= start && num >= arr[start] && num <= arr[end]) {
+            if (arr[end].equals(arr[start])) {
+                break;
+            }
+            i++;
+            base =  (int)(start +
+                    1.0 * (end - start) / (arr[end] - arr[start])
+                    * (num - arr[start]));
+
+            System.out.println("Base: " + base);
+
+            if (arr[base] == num) {
+                System.out.println("Кол-во итераций: " + i);
+                return base;
+            } else if(arr[base] < num) {
+                start = base + 1;
+            } else {
+                end = base - 1;
+            }
+        }
+        System.out.println("Кол-во итераций: " + i);
+        if (arr[start] == num) {
+            return start;
+        }
         return -1;
     }
 }
