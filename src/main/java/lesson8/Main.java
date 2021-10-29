6db39e05432676d822b811faebdded716ca1677e
karachev.aleksandr <XN}O81Q.&AYX48F6^,@lbzo1xfiQMW,8KEP!^U'hg;~NtcB~>
2021-10-29 19:50:09 +0300

    homework 8, fix comments

--- src/main/java/lesson8/Main.java
+++ src/main/java/lesson8/Main.java
@@ -3,14 +3,14 @@ package lesson8;
 public class Main {
 
     public static void main(String[] args) {
-        var hashTable = new HashTableChainImpl<>(2); //5*2
+        var hashTable = new HashTableChainImpl<>(2); //2*2
 
-        hashTable.put(new Product(1, "Orange"), 150);  //1
-        hashTable.put(new Product(77, "Banana"), 100); //7
-        hashTable.put(new Product(67, "Carrot"), 228); //8
-        hashTable.put(new Product(60, "Lemon"), 250);   //0
-        hashTable.put(new Product(51, "Milk"), 120);    //2
-        hashTable.put(new Product(21, "Potato"), 67);   //3
+        hashTable.put(new Product(1, "Orange"), 150);
+        hashTable.put(new Product(77, "Banana"), 100);
+        hashTable.put(new Product(67, "Carrot"), 228);
+        hashTable.put(new Product(60, "Lemon"), 250);
+        hashTable.put(new Product(51, "Milk"), 120);
+        hashTable.put(new Product(21, "Potato"), 67);
 
         System.out.println("put [Orange - 150, Banana - 100, Carrot - 228, Lemon - 250, Milk - 120, Potato - 67]");
         hashTable.display();
@@ -30,7 +30,7 @@ public class Main {
         System.out.println("Cost potato is " + hashTable.get(new Product(21, "Potato")));
         System.out.println("Cost banana is " + hashTable.get(new Product(77, "Banana")));
         System.out.println("Cost carrot is " + hashTable.get(new Product(67, "Carrot")));
-//
+
         hashTable.remove(new Product(77, "Banana"));
         hashTable.remove(new Product(21, "Potato"));
         hashTable.remove(new Product(10, "Cherry"));
@@ -38,18 +38,18 @@ public class Main {
         System.out.println();
         System.out.println("remove [Banana, Potato, Cherry]");
         System.out.println();
-//
+
         System.out.println("Cost potato is " + hashTable.get(new Product(21, "Potato")));
         System.out.println("Cost banana is " + hashTable.get(new Product(77, "Banana")));
         System.out.println("Cost carrot is " + hashTable.get(new Product(67, "Carrot")));
 
         hashTable.display();
         System.out.println("Size is " + hashTable.size());
-//
+
         System.out.println();
         hashTable.put(new Product(47, "Pineapple"), 228);
         System.out.println("put [Pineapple - 228]");
-//
+
         hashTable.display();
         System.out.println("Size is " + hashTable.size());
     }
